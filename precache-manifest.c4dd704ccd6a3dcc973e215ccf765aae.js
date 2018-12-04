1ba4790329c42090c07dc9050f0975ff6d5ab5fe
aoodaood <36750672+aoodaood@users.noreply.github.com>
2018-12-04 16:18:48 +0700

    Add files via upload

---
+++ precache-manifest.c4dd704ccd6a3dcc973e215ccf765aae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1c200df2db9abb9a6a38",
+    "url": "/static/js/main.1c200df2.chunk.js"
+  },
+  {
+    "revision": "0af77a0b6ef112a62be7",
+    "url": "/static/js/1.0af77a0b.chunk.js"
+  },
+  {
+    "revision": "1c200df2db9abb9a6a38",
+    "url": "/static/css/main.c29971c4.chunk.css"
+  },
+  {
+    "revision": "aca0e9faaa0c023e7a75c3a7958d6ad0",
+    "url": "/index.html"
+  }
+];
